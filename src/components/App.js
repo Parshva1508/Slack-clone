79749cb69cc9d269403db02f5387a4e2caa35a08
Parshva Vora <parshvavora1999@gmail.com>
2020-09-15 18:01:48 +0530

    Adding Files

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        Hello World Application.
+      </header>
+    </div>
+  );
+}
+
+export default App;
