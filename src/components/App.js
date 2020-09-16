25bd0240ffc0c8134f4ea3aea72a11c1f14b59db
Parshva Vora <parshvavora1999@gmail.com>
2020-09-16 17:52:55 +0530

    changes saved

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
+import { SignIn } from './';
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         Hello World Application.
+        <SignIn/>
       </header>
     </div>
   );
