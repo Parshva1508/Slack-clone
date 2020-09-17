ab69d379f7e7c2bf361f317d5167ac87dca90204
Parshva Vora <parshvavora1999@gmail.com>
2020-09-17 18:38:52 +0530

    changes saved

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,27 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { SignIn } from './';
+import { Switch, Route } from 'react-router-dom';
 
+function Home() {
+  return <div>Home</div>;
+}
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        Hello World Application.
-        <SignIn/>
-      </header>
-    </div>
-  );
+function Some() {
+  return <div>Some</div>;
+}
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <Switch>
+          <Route exact path="/" component={SignIn} />
+          <Route exact path="/home" component={Home} />
+          <Route exact path="/some" component={Some} />
+        </Switch>
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/components/SignIn.js
+++ src/components/SignIn.js
@@ -1,13 +1,14 @@
 import React, { Component } from 'react';
 
-class SignIn extends Component{
-    render(){
-        return(
-            <div>
-                Sign In
-            </div>
-        );
-    }
+class SignIn extends Component {
+  render() {
+    return (
+      <div className="Signin-form">
+        <h1>SIGN IN</h1>
+        <button> Sign In with Google!!</button>
+      </div>
+    );
+  }
 }
 
 export default SignIn;
